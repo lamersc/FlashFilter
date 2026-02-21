5344013b79e444865be2a4fdace954b87e09b2e9
Matthew Power <m13power@gmail.com>
2026-02-21 10:52:28 -0700

    feat: simple pixel grouping

--- censor-algorithm/main.py
+++ censor-algorithm/main.py
@@ -1,13 +1,54 @@
 import cv2
 import sys
+from dataclasses import dataclass
 import numpy as np
+
+# Quick reminder: HSL != HSV
+
+@dataclass
+class PixelGroup: 
+    center: np.ndarray # Average position of our pixels (2 channel YX)
+    color: np.ndarray # Average color of our pixels (3 channel HSL)
+    count: int # Number of pixels in group (used for averaging)
+    
 
 # Global variable to store the previous frame
 previous_frame = None
 
+def update_average(current: np.ndarray, sample: np.ndarray, count: int) -> np.ndarray:
+    """Update the average color of a group with a new pixel."""
+    return current + (sample - current) / count
+
+def segment_frame(frame_hsl: np.ndarray, distance: int) -> np.ndarray:
+    """
+    Imagine fuzzy selecting every distinct block of color in a frame.
+    This function will return a list of groups of pixels each group has the following info:
+    """
+    groups = []
+    for y in range(frame_hsl.shape[0]):
+        for x in range(frame_hsl.shape[1]):
+            pos = np.array([y, x])
+            pixel = frame_hsl[y, x]
+            assigned = False
+            for group in groups:
+                if np.linalg.norm(pixel - group.center) < distance:
+                    group.center = update_average(group.center, pos, group.count)
+                    group.color = update_average(group.color, pixel, group.count)
+                    group.count += 1
+                    assigned = True
+                    break
+            if not assigned:
+                groups.append(PixelGroup(center=pos, color=pixel, count=1))
+
+
+# We expect frame format to be BGR
 def process_frame(frame):
-    """Adaptive brightness clamping scaled by optical flow motion."""
+    """Pixel delta clamping based on where in the color space we move from/to."""
     global previous_frame
+
+    # First we want to move to a color space where we can more nicely understand
+    # the relationships of channel changes 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
 
     # On first call, store the frame and return it
     if previous_frame is None:
@@ -15,6 +56,7 @@ def process_frame(frame):
         return frame
 
     max_delta = 30
+
     # Convert frames to int16 for arithmetic
     current = frame.astype(np.int16)
     previous = previous_frame.astype(np.int16)
